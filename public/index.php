7fde632b867e4b4e36c1754c89f53b102ff1ae0a
laetitiaquintin-83 <laetitia.quintin@laplateforme.io>
2025-11-19 14:38:26 +0100

    Ajout de phpdotenv, création des variables d'envirronements dans le fichier database php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once __DIR__ .'/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ ."/../");
+$dotenv->safeLoad();
 // Importation des classes avec namespaces pour éviter les conflits de noms
 use Core\Router;
 
